ad5ccbedf6bd449139a61fb52fc30e8c92134d3a
Satya <satran004@gmail.com>
2020-05-27 21:00:15 +0800

    Implement retry incase of connection failure

--- src/main/java/com/bloxbean/kafka/connectors/web3/blocks/BlockSourceTask.java
+++ src/main/java/com/bloxbean/kafka/connectors/web3/blocks/BlockSourceTask.java
@@ -1,5 +1,6 @@
 package com.bloxbean.kafka.connectors.web3.blocks;
 
+import com.bloxbean.kafka.connectors.web3.exception.Web3Exception;
 import com.bloxbean.kafka.connectors.web3.util.ConfigConstants;
 import com.bloxbean.kafka.connectors.web3.client.Web3RpcClient;
 import com.bloxbean.kafka.connectors.web3.util.HexConverter;
@@ -18,12 +19,16 @@ import static com.bloxbean.kafka.connectors.web3.util.ConfigConstants.*;
 
 public class BlockSourceTask extends SourceTask {
     private static Logger logger = LoggerFactory.getLogger(BlockSourceTask.class);
+    private static int RETRY_THRESHOLD = 10;
+    private static int DELAY_AFTER_RETRY_THRESHOLD = 30 * 1000; //Retry every 30 sec after retry threshhold
 
     private BlockSourceConfig config;
     private Web3RpcClient web3RpcClient;
 
     private long newBlockWaitTime = 0;
     private long blockNumberOffset;
+
+    private int retryCounter;
 
     public String version() {
         return ConfigConstants.VERSION;
@@ -76,10 +81,22 @@ public class BlockSourceTask extends SourceTask {
             logger.info("Successfully fetched block : {} ", jsonObject.getString("number"));
 
             blockNumberOffset++;
+            retryCounter = 0;
 
             return sourceRecords;
-        } catch (Web3ConnectorException ex) {
-            logger.error("Error getting data through web3 client", ex);
+        } catch (Web3ConnectorException | Web3Exception ex) {
+            if(!isRetryThresholdReachedAndwaitDuringRetry()) {
+                logger.error("Error getting data through web3 client", ex);
+            } else {
+                logger.error("Error getting data through web3 client", ex.getMessage());
+            }
+            return null;
+        } catch (Exception ex) {
+            if(!isRetryThresholdReachedAndwaitDuringRetry()) {
+                logger.error("System Error", ex);
+            } else {
+                logger.error("System Error", ex.getMessage());
+            }
             return null;
         }
     }
@@ -131,6 +148,17 @@ public class BlockSourceTask extends SourceTask {
         Map<String, String> map = new HashMap<>();
         map.put(LAST_FETCHED_BLOCK_NUMBER, String.valueOf(blockNumber));
         return map;
+    }
+
+    private boolean isRetryThresholdReachedAndwaitDuringRetry() throws InterruptedException {
+        retryCounter ++;
+        if(retryCounter >= RETRY_THRESHOLD) {
+            Thread.sleep(DELAY_AFTER_RETRY_THRESHOLD);
+            return true;
+        } else {
+            Thread.sleep(10000);
+            return false;
+        }
     }
 
     public void stop() {
--- src/main/java/com/bloxbean/kafka/connectors/web3/events/EventsSourceTask.java
+++ src/main/java/com/bloxbean/kafka/connectors/web3/events/EventsSourceTask.java
@@ -2,6 +2,7 @@ package com.bloxbean.kafka.connectors.web3.events;
 
 import com.bloxbean.kafka.connectors.web3.client.Web3RpcClient;
 import com.bloxbean.kafka.connectors.web3.exception.Web3ConnectorException;
+import com.bloxbean.kafka.connectors.web3.exception.Web3Exception;
 import com.bloxbean.kafka.connectors.web3.util.ConfigConstants;
 import com.bloxbean.kafka.connectors.web3.util.HexConverter;
 import com.bloxbean.kafka.connectors.web3.util.StringUtil;
@@ -20,12 +21,16 @@ import static com.bloxbean.kafka.connectors.web3.util.ConfigConstants.WEB3_RPC_U
 
 public class EventsSourceTask extends SourceTask {
     private static Logger logger = LoggerFactory.getLogger(EventsSourceTask.class);
+    private static int RETRY_THRESHOLD = 10;
+    private static int DELAY_AFTER_RETRY_THRESHOLD = 30 * 1000; //Retry every 30 sec after retry threshhold
 
     private EventsSourceConfig config;
     private Web3RpcClient web3RpcClient;
 
     private long newBlockWaitTime = 0;
     private long blockNumberOffset;
+
+    private int retryCounter;
 
     public String version() {
         return ConfigConstants.VERSION;
@@ -94,8 +99,19 @@ public class EventsSourceTask extends SourceTask {
             blockNumberOffset++;
 
             return sourceRecords;
-        } catch (Web3ConnectorException ex) {
-            logger.error("Error getting data through web3 client", ex);
+        } catch (Web3ConnectorException | Web3Exception ex) {
+            if(!isRetryThresholdReachedAndwaitDuringRetry()) {
+                logger.error("Error getting data through web3 client", ex);
+            } else {
+                logger.error("Error getting data through web3 client", ex.getMessage());
+            }
+            return null;
+        } catch (Exception ex) {
+            if(!isRetryThresholdReachedAndwaitDuringRetry()) {
+                logger.error("System Error", ex);
+            } else {
+                logger.error("System Error", ex.getMessage());
+            }
             return null;
         }
     }
@@ -194,6 +210,17 @@ public class EventsSourceTask extends SourceTask {
         Map<String, String> map = new HashMap<>();
         map.put(LAST_FETCHED_BLOCK_NUMBER, String.valueOf(blockNumber));
         return map;
+    }
+
+    private boolean isRetryThresholdReachedAndwaitDuringRetry() throws InterruptedException {
+        retryCounter ++;
+        if(retryCounter >= RETRY_THRESHOLD) {
+            Thread.sleep(DELAY_AFTER_RETRY_THRESHOLD);
+            return true;
+        } else {
+            Thread.sleep(10000);
+            return false;
+        }
     }
 
     public void stop() {
